94a679adc6dafc42cf0571d68d0015e32c4db43e
Sam Hatfield <samuel.e.hatfield@gmail.com>
2015-11-17 15:16:12 -0600

    Drone uses python 3

--- testing/integration-test.py
+++ testing/integration-test.py
@@ -3,13 +3,13 @@
 # This is meant to perform an integration test for mantl, testing against many different providers
 # destroying the infrastructure on each one when the test is completed. If any of the providers fail, the whole test
 # fails. The test should be based on the sample files, since that is what we are providing for users.
-# 
-# IDEAS: 
+#
+# IDEAS:
 # the security-setup script should be run automatically, with test:$(/dev/random) being the password.
 # the exit code should be init'd to 0, and there should be a 'try/catch/finally' block within the script
-# the finally part of the 'try/catch/finally' would be destroying the infrastructure. 
+# the finally part of the 'try/catch/finally' would be destroying the infrastructure.
 # OR
 # the tests for each provider could be a separate process/thread, and then aggregate the results into report
 # I like this better
 
-print "Hello, Drone"
+print("Hello, Drone")
