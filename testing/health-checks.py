76ac33c9217b95b00f1e8e5d5378cffd5f783f28
Sam Hatfield <samuel.e.hatfield@gmail.com>
2015-12-02 15:44:54 -0600

    Changed to drone AWS credentials, and finished basic health checks

--- testing/health-checks.py
+++ testing/health-checks.py
@@ -1,6 +1,37 @@
 #!/usr/bin/env python
-
+from __future__ import print_function
 import sys
+import json
+
+if sys.version_info[0] > 2:
+    from urllib.request import urlopen
+else:
+    from urllib import urlopen
+
+
+def node_health_check(node_address):
+    url = "http://" + node_address + "/consul/v1/health/state/any"
+    f = urlopen(url)
+
+    health_checks = json.loads(f.read().decode('utf8'))
+
+    for check in health_checks:
+        if check['Status'] != "passing":
+            print(check['Name'] + ": not passing. Exiting now")
+            sys.exit(1)
+        else:
+            print(check['Name'] + ": passing. Continuing")
+
+
+def cluster_health_check(ip_addresses):
+    for node_address in ip_addresses:
+        print("Testing node at IP: " + node_address)
+        node_health_check(node_address)
+        print("Done testing " + node_address)
+
 
 if __name__ == "__main__":
+    address_list = ["192.168.242.55"]
+    cluster_health_check(address_list)
+    print("Health check finished. Exiting now")
     sys.exit(0)
