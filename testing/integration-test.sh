5755aba0c6c2b1547bc64137c9dbfb6d68831c4d
Sam Hatfield <samuel.e.hatfield@gmail.com>
2015-12-02 09:54:30 -0600

    Added more exit codes (w/notes), leveraging Drone matrix builds feature

--- testing/integration-test.sh
+++ testing/integration-test.sh
@@ -3,14 +3,12 @@
 # This is meant to perform an integration test for mantl, testing against many different providers
 # destroying the infrastructure on each one when the test is completed. If any of the providers fail, the whole test
 # fails. The test should be based on the sample files, since that is what we are providing for users.
-#
-# IDEAS:
-# the security-setup script should be run automatically, with test:$(/dev/random) being the password.
-# the exit code should be init'd to 0, and there should be a 'try/catch/finally' block within the script
-# the finally part of the 'try/catch/finally' would be destroying the infrastructure.
-# OR
-# the tests for each provider could be a separate process/thread, and then aggregate the results into report
-# I like this better
+
+## EXIT_CODE meanings:
+## 00  success
+## 01  docker_launch failed
+## 10  terraform destroy failed
+## 11  docker_launch and terraform destroy failed
 
 EXIT_CODE=0 # passing until proven failed
 
@@ -19,11 +17,6 @@ mkdir /ssh
 ssh-keygen -t rsa -N '' -f /ssh/id_rsa
 ./security-setup
 
-ln -s -f terraform.sample.yml terraform.yml
-
-## for each provider, we link the file we are testing to where the docker launch script expects it to be
-ln -s -f testing/aws.tf terraform.tf
-
-./docker_launch.sh || EXIT_CODE=1
-terraform destroy -force -state=$TERRAFORM_STATE_ROOT/terraform.tfstate
+./docker_launch.sh || EXIT_CODE=$((EXIT_CODE+1))
+terraform destroy -force -state=$TERRAFORM_STATE_ROOT/terraform.tfstate || EXIT_CODE=$((EXIT_CODE+10))
 exit $EXIT_CODE
