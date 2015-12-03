5d21af5bae83eceb90650129f1d75cbe558d6c21
Sam Hatfield <samuel.e.hatfield@gmail.com>
2015-12-03 13:42:14 -0600

    Only do health checks on control hosts, duh

--- testing/integration-test.sh
+++ testing/integration-test.sh
@@ -13,8 +13,8 @@ ssh-keygen -t rsa -N '' -f /ssh/id_rsa
 # it currently makes an assumption that the build is in a docker container
 ./docker_launch.sh || EXIT_CODE=1
 
-hosts=$(plugins/inventory/terrform.py --hostfile | awk '/([0-9]*\.){3}[0-9]/ {print $1}')
+control_hosts=$(plugins/inventory/terrform.py --hostfile | awk '/control/ {print $1}')
 
-testing/health-checks.py $hosts || EXIT_CODE=1
+testing/health-checks.py $control_hosts || EXIT_CODE=1
 terraform destroy -force -state=$TERRAFORM_STATE_ROOT/terraform.tfstate || EXIT_CODE=1
 exit $EXIT_CODE
