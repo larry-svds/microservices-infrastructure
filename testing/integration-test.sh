ef92a088b53700a44380736f70e2bf8b47e0f116
Sam Hatfield <samuel.e.hatfield@gmail.com>
2015-12-07 15:46:13 -0600

    Cp'd docker_launch logic into integration test directly, with retries

--- testing/integration-test.sh
+++ testing/integration-test.sh
@@ -5,17 +5,28 @@
 EXIT_CODE=0 # passing until proven failed
 
 ## security setup
-mkdir /ssh
-ssh-keygen -t rsa -N '' -f /ssh/id_rsa
+ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa
+eval `ssh-agent -s` && ssh-add ~/.ssh/id_rsa
 ./security-setup
 
 # this section needs to make fewer assumptions of the build env
 # it currently makes an assumption that the build is in a docker container
 
 ## LOOP terraform's build step 3 times, to allow for terraform bugs to sort themselves out
-./docker_launch.sh || EXIT_CODE=1
+terraform get || EXIT_CODE=1
+for i in `seq 1 3`
+do
+	terraform apply -state=$TERRAFORM_STATE_ROOT/terraform.tfstate || EXIT_CODE=1
+	if [ "$EXIT_CODE" -ne 1 ]
+	then
+		break
+	fi
+done
 
-control_hosts=$(plugins/inventory/terrform.py --hostfile | awk '/control/ {print $1}')
+ansible-playbook playbooks/wait-for-hosts.yml --private-key ~/.ssh/id_rsa || EXIT_CODE=1
+ansible-playbook terraform.yml --extra-vars=@security.yml --private-key ~/.ssh/id_rsa || EXIT_CODE=1
+
+control_hosts=$(plugins/inventory/terraform.py --hostfile | awk '/control/ {print $1}')
 
 testing/health-checks.py $control_hosts || EXIT_CODE=1
 terraform destroy -force -state=$TERRAFORM_STATE_ROOT/terraform.tfstate || EXIT_CODE=1
