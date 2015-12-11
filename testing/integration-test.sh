26dedc4df228affff9609e8b9c596ee6fbd354c0
Sam Hatfield <samuel.e.hatfield@gmail.com>
2015-12-11 12:36:56 -0600

    Refactored retry logic into function, applied it to ansible calls

--- testing/integration-test.sh
+++ testing/integration-test.sh
@@ -3,6 +3,7 @@
 ## This integration test assumes that ./terraform.yml and ./terraform.tf are already in place
 
 EXIT_CODE=0 # passing until proven failed
+RETRY_ATTEMPTS=3
 
 ## security setup
 ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa
@@ -12,25 +13,29 @@ eval `ssh-agent -s` && ssh-add ~/.ssh/id_rsa
 # this section needs to make fewer assumptions of the build env
 # it currently makes an assumption that the build is in a docker container
 
+function retry_command() {
+	for i in `seq 1 $RETRY_ATTEMPTS`
+	do
+		eval $1
+		RETRY=$?
+		if [ $RETRY -eq 0 ]
+		then
+			break
+		fi
+	done
+	if [ $RETRY -ne 0 ] # if terraform fails three times, fail the test
+	then
+		EXIT_CODE=1
+	fi
+}
+
 ## LOOP terraform's build step 3 times, to allow for terraform bugs to sort themselves out
 terraform get
-for i in `seq 1 3`
-do
-	terraform apply -state=$TERRAFORM_STATE_ROOT/terraform.tfstate
-	APPLY_RETRY=$?
-	if [ $APPLY_RETRY -eq 0 ]
-	then
-		break
-	fi
-done
+retry_command "terraform apply -state=$TERRAFORM_STATE_ROOT/terraform.tfstate"
 
-if [ $APPLY_RETRY -ne 0 ] # if terraform fails three times, fail the test
-then
-	EXIT_CODE=1
-fi
 
-ansible-playbook playbooks/wait-for-hosts.yml --private-key ~/.ssh/id_rsa || EXIT_CODE=1
-ansible-playbook terraform.yml --extra-vars=@security.yml --private-key ~/.ssh/id_rsa || EXIT_CODE=1
+ansible-playbook playbooks/wait-for-hosts.yml --private-key ~/.ssh/id_rsa
+retry_command "ansible-playbook terraform.yml --extra-vars=@security.yml --private-key ~/.ssh/id_rsa"
 
 control_hosts=$(plugins/inventory/terraform.py --hostfile | awk '/control/ {print $1}')
 
@@ -43,4 +48,5 @@ do
 	terraform destroy -force -state=$TERRAFORM_STATE_ROOT/terraform.tfstate
 	DESTROY=$?
 done
+
 exit $EXIT_CODE
